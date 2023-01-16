eb3d6eeb65506b776b8e2eeb776841ce6ee58ee0
sappiah085 <sappiah085@st.ug.edu.gh>
2023-01-16 14:11:22 +0100

    video chat app with vanilla js and WEBRTC

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-document.querySelector(".create").addEventListener("click", async (e) => {
-  e.preventDefault();
-  window.location = `/add.html?roomID=`;
-});
+// document.querySelector(".create").addEventListener("click", async (e) => {
+//   e.preventDefault();
+//   window.location = `/add.html?roomID=`;
+// });
